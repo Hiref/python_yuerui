378505a02f8117d798b6231db7ed31aa7dbe8b48
root <you@example.com>
2018-04-02 08:43:14 +0800

    first commit

---
+++ 06day/01-个人信息.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+Name = "寒月瑞"
+age = 20
+wight = 113.3
+height = 180
+    #male男　female女
+sex ='male'
+socialstatus = "一百块"
+ 
+print(Name)
+print(age)
+print(wight)
+print(height)
+print(sex)
+print(socialstatus)   
---
+++ 06day/02-练习变量.py
@@ -0,0 +1,7 @@
+#coding=utf-8
+name
+first_name="韩"
+last_name="跃瑞"
+print(first_name+last_name)
+
+
